0ce2c12b0236c347633081d1a4c7d88d68a6d1ff
Mark Allender <mark.allender@gmail.com>
2020-12-01 09:34:18 -0600

    add day 1 of 2020

---
+++ 2020/day1/day1.py
@@ -0,0 +1,21 @@
+import itertools
+import operator
+import functools
+
+# takes input and finds the two numbers that
+# add up to 2020
+
+def part1(data, n):
+    for i in itertools.combinations(data, n):
+        if sum(i) == 2020:
+            val = functools.reduce(operator.mul, i)
+            print (val)
+            break
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        data = f.read().splitlines()
+
+    data = list(map(int, data))
+    part1(data, 2)
+    part1(data, 3)
