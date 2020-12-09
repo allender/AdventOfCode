69501329ae584f918e50e24a9d18a72ac207e676
Mark Allender <mark.allender@gmail.com>
2020-12-09 13:33:09 -0600

    More concise solution

--- 2020/day9/day9.py
+++ 2020/day9/day9.py
@@ -35,19 +35,11 @@ def find_unique(data):
             return (data[end])
 
 def find_weakness(data, num):
-    for start in range(0, len(data)): 
-        for val in itertools.accumulate(data[start:], operator.add):
-            if val == num:
-                start_loc = start
-                while val > 0:
-                    start += 1
-                    val -= data[start]
-                weakness = min(data[start_loc:start]) + max(data[start_loc:start])
-                return weakness
-
-            if val > num:
-                break
-
+    end = data.index(num)
+    for start in range(0, end): 
+        result = [ i for i, val in enumerate(itertools.accumulate(data[start:end], operator.add)) if val == num ]
+        if result:
+            return (min(data[start:start+result[0]]) + max(data[start:start+result[0]]))
 
 if __name__ == '__main__':
     with open('input.txt') as f:
