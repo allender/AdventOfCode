b0a5f9959bec9116d6f02c10bdcfeb9adb0dda2f
Mark Allender <mark.allender@gmail.com>
2020-12-22 12:21:00 -0600

    day 21 optimizatino

--- 2020/day22/day22.py
+++ 2020/day22/day22.py
@@ -1,5 +1,7 @@
 import re
 import queue
+import time
+import sys
 
 test_data="""Player 1:
 9
@@ -19,76 +21,63 @@ Player 2:
 def play_game_part1(deck1, deck2):
     round = 1
 
-    while deck1.empty() == False and deck2.empty() == False:
-        card1 = deck1.get()
-        card2 = deck2.get()
+    while len(deck1) > 0 and len(deck2) > 0:
+        card1 = deck1.pop(0)
+        card2 = deck2.pop(0)
         if card1 > card2:
-            deck1.put(card1)
-            deck1.put(card2)
+            deck1.append(card1)
+            deck1.append(card2)
         elif card2 > card1:
-            deck2.put(card2)
-            deck2.put(card1)
+            deck2.append(card2)
+            deck2.append(card1)
         else:
             assert("cards are equal")
 
         round += 1
 
-Game_number = 1
-
-def play_game_part2(deck1, deck2):
+def play_game_part2(deck1, deck2, level):
     round = 1
-    global Game_number
-    current_game = Game_number
-
     prev_hands = {}
 
-    while deck1.empty() == False and deck2.empty() == False:
-        # have we seen a hand before?  player 1 wins
-        cards = (tuple(deck1.queue), tuple(deck2.queue))
-        if cards in prev_hands:
-            # to make player 1 win - so empty player 2's deck
-            while deck2.empty() == False:
-                deck2.get()
+    while len(deck1) > 0 and len(deck2) > 0:
+        # optimization if we are in recursion (i.e. a sub game), can
+        # we declare the winner just by looking at the cards?
+        if level > 0:
+            max1 = max(deck1)
+            max2 = max(deck2)
+            if max1 > max2:
+                return (deck1, [])
 
-            return
+        # have we seen a hand before?  player 1 wins
+        cards = (tuple(deck1), tuple(deck2))
+        if cards in prev_hands:
+            deck2 = []
+            return (deck1, [])
 
         # keep track of previous decks
         prev_hands[cards] = 1
 
         # get cards
-        card1 = deck1.get()
-        card2 = deck2.get()
+        card1 = deck1.pop(0)
+        card2 = deck2.pop(0)
 
         # need to determine what to do based on rules of recursive.  Each
         # player must have at least as many cards as what he drew
-        if len(deck1.queue) >= card1 and len(deck2.queue) >= card2:
-            new_deck1 = queue.Queue()
-            new_deck2 = queue.Queue()
-            for idx in range(card1):
-                new_deck1.put(deck1.queue[idx])
-            for idx in range(card2):
-                new_deck2.put(deck2.queue[idx])
-            Game_number += 1
-            play_game_part2(new_deck1, new_deck2)
-            if len(new_deck2.queue) == 0:
-                deck1.put(card1)
-                deck1.put(card2)
+        if len(deck1) >= card1 and len(deck2) >= card2:
+            d1, d2 = play_game_part2(deck1[:card1], deck2[:card2], level + 1)
+            if len(d2) == 0:
+                deck1.extend([card1, card2])
             else:
-                deck2.put(card2)
-                deck2.put(card1)
+                deck2.extend([card2, card1])
         else:
             if card1 > card2:
-                deck1.put(card1)
-                deck1.put(card2)
+                deck1.extend([card1, card2])
             elif card2 > card1:
-                deck2.put(card2)
-                deck2.put(card1)
-            else:
-                assert("cards are equal")
+                deck2.extend([card2, card1])
 
         round += 1
 
-    assert(len(deck1.queue) == 0 or len(deck2.queue) == 0)
+    return (deck1, deck2)
 
 
 if __name__ == '__main__':
@@ -97,50 +86,48 @@ if __name__ == '__main__':
 
     data = input_data.split('\n')
 
-    deck1 = queue.Queue()
-    deck2 = queue.Queue()
+    deck1 = []
+    deck2 = []
 
     index = 1
     while data[index] != '':
-        deck1.put(int(data[index]))
+        deck1.append(int(data[index]))
         index += 1
 
     index += 2
     while data[index] != '':
-        deck2.put(int(data[index]))
+        deck2.append(int(data[index]))
         index += 1
 
     # play_game_part1(deck1, deck2)
     play_game_part1(deck1, deck2)
-    deck1_list = list(deck1.queue)
-    deck2_list = list(deck2.queue)
 
-    score1 = sum( [ (len(deck1_list) - i) * val for i, val in enumerate(deck1_list) ] ) 
-    score2 = sum( [ (len(deck2_list) - i) * val for i, val in enumerate(deck2_list) ] ) 
+    score1 = sum( [ (len(deck1) - i) * val for i, val in enumerate(deck1) ] ) 
+    score2 = sum( [ (len(deck2) - i) * val for i, val in enumerate(deck2) ] ) 
 
     print(score1, score2)
 
     # reinitialize everything
-    deck1 = queue.Queue()
-    deck2 = queue.Queue()
+    deck1 = []
+    deck2 = []
 
     index = 1
     while data[index] != '':
-        deck1.put(int(data[index]))
+        deck1.append(int(data[index]))
         index += 1
 
     index += 2
     while data[index] != '':
-        deck2.put(int(data[index]))
+        deck2.append(int(data[index]))
         index += 1
 
-    play_game_part2(deck1, deck2)
+    start = time.time()
+    deck1, deck2 = play_game_part2(deck1, deck2, 0)
+    end = time.time()
+    print(end - start)
 
-    deck1_list = list(deck1.queue)
-    deck2_list = list(deck2.queue)
-
-    score1 = sum( [ (len(deck1_list) - i) * val for i, val in enumerate(deck1_list) ] ) 
-    score2 = sum( [ (len(deck2_list) - i) * val for i, val in enumerate(deck2_list) ] ) 
+    score1 = sum( [ (len(deck1) - i) * val for i, val in enumerate(deck1) ] ) 
+    score2 = sum( [ (len(deck2) - i) * val for i, val in enumerate(deck2) ] ) 
 
     print(score1, score2)
 
