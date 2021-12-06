898543d732b6f370f69417b9a2470ec28d0e38b2
Mark Allender <mark.allender@gmail.com>
2021-12-06 10:39:50 -0600

    Day 6

---
+++ 2021/day6/day6.py
@@ -0,0 +1,26 @@
+from aocd import lines
+from collections import Counter
+
+test_lines = [
+    '3,4,3,1,2'
+]
+
+def age_fish(age_data, days):
+    for _ in range(days):
+        # take first element of the list (age 0), add
+        # in a new fish if need
+        fish_age = age_data.pop(0)
+        age_data.append(fish_age)
+        age_data[6] += data[-1]
+        
+
+if __name__ == '__main__': 
+    numbers = list(map(int, lines[0].split(',')))
+
+    # create Counter object to keep track of the count
+    # of fish at a given age.  and then convert to
+    # list that has the counts at the age locations
+    age_counter = Counter(numbers)
+    data = [ age_counter[i] for i in range(9) ]
+    age_fish(data, 256)
+    print(sum(data))
