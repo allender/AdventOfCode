26e34b4f28dcf366866d3199f8b0d39b0945bb17
Mark Allender <mark.allender@gmail.com>
2021-12-06 10:43:22 -0600

    day 6 with both parts together

--- 2021/day6/day6.py
+++ 2021/day6/day6.py
@@ -1,17 +1,20 @@
 from aocd import lines
 from collections import Counter
+from typing import List
 
 test_lines = [
     '3,4,3,1,2'
 ]
 
-def age_fish(age_data, days):
+def age_fish(age_data : List, days) -> List:
     for _ in range(days):
         # take first element of the list (age 0), add
         # in a new fish if need
         fish_age = age_data.pop(0)
         age_data.append(fish_age)
         age_data[6] += data[-1]
+        
+    return age_data
         
 
 if __name__ == '__main__': 
@@ -22,5 +25,5 @@ if __name__ == '__main__':
     # list that has the counts at the age locations
     age_counter = Counter(numbers)
     data = [ age_counter[i] for i in range(9) ]
-    age_fish(data, 256)
-    print(sum(data))
+    print(sum(age_fish(data, 18)))
+    print(sum(age_fish(data, 256)))
